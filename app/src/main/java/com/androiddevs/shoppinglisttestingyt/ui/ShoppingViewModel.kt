d299bde9c88326479fa670cd841aff5163d57f99
Abhinay <Abhinay>
2023-10-20 00:21:36 +0530

    Architectural Skeleton

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingViewModel.kt
@@ -1,8 +1,16 @@
 package com.androiddevs.shoppinglisttestingyt.ui
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.others.Event
+import com.androiddevs.shoppinglisttestingyt.others.Resource
 import com.androiddevs.shoppinglisttestingyt.repositories.ShoppingRepository
+import kotlinx.coroutines.launch
 
 /**
  * Created by Abhinay on 19/10/23.
@@ -10,5 +18,38 @@ import com.androiddevs.shoppinglisttestingyt.repositories.ShoppingRepository
 class ShoppingViewModel @ViewModelInject constructor(
     private val repository: ShoppingRepository
 ) : ViewModel() {
+
+    val shoppingItems = repository.observeAllShoppingItems()
+
+    val totalPrice = repository.observeTotalPrice()
+
+    private val _images = MutableLiveData<Event<Resource<ImageResponse>>>()
+    val images: LiveData<Event<Resource<ImageResponse>>> = _images
+
+    private val _curImageUrl = MutableLiveData<String>()
+    val curImageUrl: LiveData<String> = _curImageUrl
+
+    private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+
+    fun setCurImageUrl(url: String) {
+        _curImageUrl.postValue(url)
+    }
+
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.deleteShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.insertShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+
+    }
+
+    fun searchImage(imageQuery: String) {
+
+    }
 
 }
