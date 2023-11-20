aab5ebf36dc54512bf1c933b577a33d959a38c66
Abhinay <Abhinay>
2023-11-20 13:53:25 +0530

    Testing Image

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/di/AppModule.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.androiddevs.shoppinglisttestingyt.di
 
 import android.content.Context
 import androidx.room.Room
+import com.androiddevs.shoppinglisttestingyt.R
 import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
 import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItemDatabase
 import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
@@ -9,6 +10,8 @@ import com.androiddevs.shoppinglisttestingyt.others.Constants.BASE_URL
 import com.androiddevs.shoppinglisttestingyt.others.Constants.DATABASE_NAME
 import com.androiddevs.shoppinglisttestingyt.repositories.DefaultShoppingRepository
 import com.androiddevs.shoppinglisttestingyt.repositories.ShoppingRepository
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -55,4 +58,13 @@ object AppModule {
             .build()
             .create(PixabayAPI::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+        )
 }
